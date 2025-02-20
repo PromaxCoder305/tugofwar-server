d47c4090137ace37a158e88e0f259d458802057c
promaxpr <info.promaxpr@gmail.com>
2025-02-20 14:59:42 +0530

    12

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -2,8 +2,8 @@ const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
   cloud_name: 'dqdpmbrej',
-  api_key: '764959737765557',
-  api_secret: '4JTV_uDBjDju2puyCOL1q_6zL98',
+  api_key: '113532974751719',
+  api_secret: 'YvwuQgko0HnHoXY9_h8VNQQGZuQ',
 });
 
 module.exports = cloudinary;
